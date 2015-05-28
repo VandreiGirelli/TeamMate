2f2a0b838a41c69fe69f899d3686146dd4ac0c1f
Rafael Fernando Rauber <rrafaelfr2@gmail.com>
2015-05-28 16:25:56 -0300

    Inicio de Validacoes

--- TeamMate/TeamMate-ejb/src/main/java/br/com/goteam/teammate/modelo/Status.java
+++ TeamMate/TeamMate-ejb/src/main/java/br/com/goteam/teammate/modelo/Status.java
@@ -1,12 +1,17 @@
 package br.com.goteam.teammate.modelo;
 
+import br.com.goteam.teammate.services.CharacterType;
+import br.com.goteam.teammate.valueobjects.CaseType;
 import java.io.Serializable;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @SequenceGenerator(allocationSize = 1, initialValue = 1,
@@ -16,6 +21,10 @@ public class Status implements Serializable {
     @Id
     @GeneratedValue
     private Long codigo;
+    @NotNull
+    @Length(min = 6, max = 50)
+    @CharacterType(value = CaseType.LETTER_SPACE)
+    @Column(nullable = false, length = 50)
     private String descricao;
     @OneToMany(mappedBy = "status")
     private List<Estado> listaEstado;
--- TeamMate/TeamMate-ejb/src/main/java/br/com/goteam/teammate/modelo/Usuario.java
+++ TeamMate/TeamMate-ejb/src/main/java/br/com/goteam/teammate/modelo/Usuario.java
@@ -2,12 +2,15 @@ package br.com.goteam.teammate.modelo;
 
 import java.io.Serializable;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @SequenceGenerator(initialValue = 1, allocationSize = 1,
@@ -17,7 +20,13 @@ public class Usuario implements Serializable {
     @Id
     @GeneratedValue(generator = "SEQ_USUARIO", strategy = GenerationType.SEQUENCE)
     private Long codigo;
+    @NotNull
+    @Length(min = 6, max = 60)
+    @Column(nullable = false, length = 60)
     private String nome;
+    @NotNull
+    @Length(min = 6, max = 60)
+    @Column(nullable = false, length = 60)
     private String senha;
     @OneToMany(mappedBy = "usuario")
     private List<Estado> listaEstado;
